ce11a36774e83f846f606d6652c2fffbf5381e20
alex <65570061+alexdelrio7@users.noreply.github.com>
2023-10-24 21:23:51 +0400

    completed exercise js 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
-};
+  // Check if the cleaned string is the same when reversed
+  const reversedStr = cleanStr.split('').reverse().join('');
+  
+  return cleanStr === reversedStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
