8aebe6a667d133e559a2b4bb160806f6512f9ddc
alex <65570061+alexdelrio7@users.noreply.github.com>
2023-10-24 21:49:58 +0400

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
 const getTheTitles = function() {
+    return books.map(book => book.title);
+}
 
-};
+// Example usage:
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
 // Do not edit below this line
 module.exports = getTheTitles;
