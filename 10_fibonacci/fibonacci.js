e2c855abba55f209d7c72e63ed7e5e337cc5e1ef
alex <65570061+alexdelrio7@users.noreply.github.com>
2023-10-24 21:38:48 +0400

    completed js 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let number = n
+        if (isNaN(n) || n < 0) {
+          return "OOPS"; // Handle negative numbers and non-numeric input
+        }
+        n = parseInt(n, 10); // Convert to a number
+      
+        if (n <= 0) {
+          return 0; // The 0th member of the sequence is 0
+        } else if (n === 1 || n === 2) {
+          return 1; // The 1st and 2nd members of the sequence are both 1
+        } else {
+          return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+      
+      
 };
 
 // Do not edit below this line
